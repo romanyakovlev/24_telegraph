38770bf16b928689cbb5d215d2ae72bc71adf55b
romanyakovlev <yakovlevroman8@gmail.com>
2017-01-29 00:29:06 +0700

    add telegraph db

--- create_db_and_table.py
+++ create_db_and_table.py
@@ -7,6 +7,6 @@ c.execute('''CREATE TABLE telegraphs
           (post_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
            header text, signature text, body text)''')
 c.execute('''INSERT INTO telegraphs(header, signature, body)
-                VALUES ('header1','title1','body1')''')
+                VALUES ('Example','Example','Example')''')
 conn.commit()
 conn.close()
