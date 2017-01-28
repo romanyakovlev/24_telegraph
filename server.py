3ab08574560f01fa21dc54cdc70788e9d3312978
romanyakovlev <yakovlevroman8@gmail.com>
2017-01-28 23:55:21 +0700

    push

--- server.py
+++ server.py
@@ -53,4 +53,6 @@ def post_page(post_id):
 
 
 if __name__ == "__main__":
-    app.run(port=8000)
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
