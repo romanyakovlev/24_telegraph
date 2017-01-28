4874a5b589156f70fa6aadae57a22f2ee4db0cf6
romanyakovlev <yakovlevroman8@gmail.com>
2017-01-29 00:06:15 +0700

    delete old db

--- create_db_and_table.py
+++ create_db_and_table.py
@@ -4,7 +4,9 @@ import sqlite3
 conn = sqlite3.connect('telegraph.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE telegraphs
-          (post_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, header text, signature text, body text)''')
-c.execute("INSERT INTO telegraphs(header, signature, body) VALUES ('header1','title1','body1')")
+          (post_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+           header text, signature text, body text)''')
+c.execute('''INSERT INTO telegraphs(header, signature, body)
+                VALUES ('header1','title1','body1')''')
 conn.commit()
 conn.close()
