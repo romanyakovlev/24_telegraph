493cd9deba9b88aeb764fa8dabff7a23f691d886
Roman Yakovlev <yakovlevroman8@gmail.com>
2017-06-04 02:40:29 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from cryptography.fernet import Fernet
 from flask import Flask
 import psycopg2
-import urlparse
+from urllib.parse import urlparse
 
 
 app = Flask(__name__)
@@ -9,4 +9,4 @@ key = b'xaoOlrrom5pcjTFytdY9pFbTBAuNG95U43qHFXv6gZw='
 f = Fernet(key)
 
 urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+url = urlparse(os.environ["DATABASE_URL"])
